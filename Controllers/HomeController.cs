ab846457e3621e4aa1951b988311be4f17928d97
gqqnbig <gqqnb2005@gmail.com>
2018-01-26 21:01:19 -0800

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VPN.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChangePassword()
+        {
+            ViewBag.IsChangeSuccessful = true;
+
+
+
+            return View("Index");
+        }
+    }
+}
