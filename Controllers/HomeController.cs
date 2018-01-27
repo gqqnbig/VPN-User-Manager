0716e01c11eb23b48836e978769e5e1b5a82b0db
gqqnbig <gqqnb2005@gmail.com>
2018-01-27 13:24:14 -0800

    Add Google ReCAPTCHA

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,10 +6,14 @@ using System.Web;
 using System.Web.Mvc;
 using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace VPN.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : AsyncController
     {
         public ActionResult Index()
         {
@@ -17,11 +21,37 @@ namespace VPN.Controllers
         }
 
         [HttpPost]
-        public ActionResult ChangePassword(string userName, string password, string newPassword)
+        public async Task<ActionResult> ChangePassword(string userName, string password, string newPassword)
         {
 
             try
             {
+                string gRecaptchaResponse = Request.Form["g-recaptcha-response"];
+
+
+                UriBuilder uriBuilder = new UriBuilder("https://www.google.com/recaptcha/api/siteverify");
+                var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+                query["secret"] = System.Configuration.ConfigurationManager.AppSettings["GoogleReCAPTCHASecretKey"];
+                query["response"] = gRecaptchaResponse;
+                uriBuilder.Query = query.ToString();
+
+                WebRequest webRequest = WebRequest.CreateHttp(uriBuilder.ToString());
+                webRequest.Method = "POST";
+                webRequest.ContentLength = 0;
+                var response = await webRequest.GetResponseAsync();
+                using (var sr = new StreamReader(response.GetResponseStream()))
+                {
+                    var jsonString = sr.ReadToEnd();
+                    var json = JObject.Parse(jsonString);
+                    if (Convert.ToBoolean(json["success"]) == false)
+                    {
+                        throw new UnauthorizedAccessException("CAPTCHA validation failed.");
+                    }
+                }
+
+
+
+
                 using (var context = new PrincipalContext(ContextType.Machine))
                 using (var user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, userName))
                 {
