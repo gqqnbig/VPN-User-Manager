6164fc35a2083df706bf1e2e3ed4cbad789083b8
gqqnbig <gqqnb2005@gmail.com>
2018-01-26 21:32:49 -0800

    Add exception handling

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,14 +20,24 @@ namespace VPN.Controllers
         public ActionResult ChangePassword(string userName, string password, string newPassword)
         {
 
-            using (var context = new PrincipalContext(ContextType.Machine))
-            using (var user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, userName))
+            try
             {
-                user.ChangePassword(password, newPassword);
+                using (var context = new PrincipalContext(ContextType.Machine))
+                using (var user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, userName))
+                {
+                    user.ChangePassword(password, newPassword);
+                }
+
+                ViewBag.PasswordMessage = "Password is changed.";
             }
-
-            ViewBag.IsChangeSuccessful = true;
-
+            catch (NullReferenceException)
+            {
+                ViewBag.PasswordMessage = "User is not found.";
+            }
+            catch (Exception e)
+            {
+                ViewBag.PasswordMessage = e.Message;
+            }
 
 
             return View("Index");
